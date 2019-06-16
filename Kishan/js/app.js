01ab26c757032ed0f123899a45455e06ae5898fb
Kishanoffcial8899 <kishanpatel3545@gmail.com>
2019-06-16 17:09:18 +0530

    last commit

---
+++ Kishan/js/app.js
@@ -0,0 +1,47 @@
+var carousel = function () {
+    $('.home-slider').owlCarousel({
+        loop: true,
+        autoplay: true,
+        margin: 50,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        nav: false,
+        autoplayHoverPause: false,
+        items: 1,
+        navText: ["<span class='ion-md-arrow-back'></span>", "<span class='ion-chevron-right'></span>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    $('.carousel-testimony').owlCarousel({
+        autoplay: true,
+        center: true,
+        loop: true,
+        items: 1,
+        margin: 30,
+        stagePadding: 0,
+        nav: false,
+        navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+};
+carousel();
